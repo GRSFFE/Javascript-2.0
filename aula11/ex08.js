6cbd02e5cdd95d9de5942ecfd78745fc3d7d5425
Germanno Resende <germannoresende@gmail.com>
2022-12-12 20:12:27 -0300

    Create new JS and HTML exercises

---
+++ aula11/ex08.js
@@ -0,0 +1,6 @@
+let vel = 80.5
+console.log(`A velocidade do seu carro é de ${vel}km/h.`)
+if (vel > 60.5) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log(`Diriga SEMPRE usando cinto de segurança!`)
---
+++ aula11/ex09.js
@@ -0,0 +1,8 @@
+let pais = 'França'
+console.log(`Vivendo em: ${pais}.`)
+if (pais == 'Brasil') {
+    console.log(`Brasileiro!`)
+}
+else {
+    console.log(`Gringo!`)
+}
