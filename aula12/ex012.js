5ff3227fc9fdce35db2579d24fca6e56f6c8df1e
Germanno Resende <germannoresende@gmail.com>
2022-12-14 00:08:34 -0300

    Create 3 new js exercises

---
+++ aula12/ex012.js
@@ -0,0 +1,8 @@
+let hora = 19
+if (hora < 12) {
+    console.log(`Agora são ${hora}h da manhã, bom dia!`)
+} else if (hora < 19) {
+    console.log(`Agora são ${hora}h da tarde, boa tarde!`)
+} else {
+    console.log(`Agora são ${hora}h da noite, boa noite!`)
+}
