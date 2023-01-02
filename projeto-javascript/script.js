5f0b2c58bfcff3890ad5f7a597d924460ed483ce
Germanno Resende <germannoresende@gmail.com>
2023-01-02 11:54:37 -0300

    Creates the website that shows the current time

--- projeto-javascript/script.js
+++ projeto-javascript/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function carregar() {
+    let msg = window.document.getElementById('msg')
+    let img = window.document.getElementById('imagem') 
+    let data = new Date()
+    let hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = 'morning.jpg'
+        document.body.style.background = '#FA8D1F'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE
+        img.src = 'tarde.jpeg'
+        document.body.style.background = '#FE984F'
+    } else {
+        //BOA NOITE
+        img.src = 'noite.jpg'
+        document.body.style.background = '#242725'
+    }
+}
