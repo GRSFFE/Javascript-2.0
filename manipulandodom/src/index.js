84a530c5261acdd8eee68a04e20f2558a737cdac
Germanno Resende <germannoresende@gmail.com>
2023-01-13 12:17:04 -0300

    add a DOM manipulation exercise

--- manipulandodom/src/index.js
+++ manipulandodom/src/index.js
@@ -1,23 +1,27 @@
-import "./style.css";
+const inputElement = document.querySelector("input");
+const buttonElement = document.querySelector("form button");
+const ulElement = document.querySelector("ul");
 
-const API_BASE_URL = "https://api.github.com/orgs/facebook/repos";
-const PAGE_SIZE = 10
-let PAGE = 1;
+buttonElement.onclick = ev => {
+  ev.preventDefault();
 
-let canFetch = true;
+  if (inputElement.value) {
+    const textElement = document.createElement("span")
+    textElement.innerHTML = inputElement.value;
 
-const reposContainer = document.getElementById('repos-container');
+    const btnElement = document.createElement("button")
+    btnElement.innerHTML = "Remover";
 
-reposContainer.addEventListener('scroll', handleScroll);
 
-function createUrl(){
-    const url = new URL(API_BASE_URL);
-    url.searchParams.set("per_page", PAGE_SIZE);
-    url.searchParams.set("page", PAGE);
+    const liElement = document.createElement("li");
+    liElement.appendChild(textElement);
+    liElement.appendChild(btnElement);
 
-    return url;
-}
+    btnElement.onclick = () => {
+        ulElement.removeChild(liElement)
+    }
 
-function handleScroll(){
-
-}
+    ulElement.appendChild(liElement);
+    inputElement.value = "";
+  }
+};
